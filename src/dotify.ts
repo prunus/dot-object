5a90cb61c3965099ef2cec7f2a3caa64aa354a81
Tony <tony.js@zoho.eu>
2021-09-17 14:57:28 -0300

    feat: add more options for customize dotify output

--- src/dotify.ts
+++ src/dotify.ts
@@ -1,19 +1,25 @@
 export interface DotifyOptions {
   arrayBracket?: boolean | [ string, string ]
   separator?: string
+  coverUpProperty?: string | [ string, string ]
 }
 
 export const dotify = ( dot: Record<string, any>, options: DotifyOptions = {} ): any => {
   const isArray = Array.isArray( dot )
-  const { arrayBracket = true, separator: propertySeparator = '.' } = options
+  const { arrayBracket = true, separator: propertySeparator = '.', coverUpProperty = [ '', '' ] } = options
   const [ startArrayBracket, closeArrayBracket ] = Array.isArray( arrayBracket ) && arrayBracket.length === 2
     ? arrayBracket
     : arrayBracket
       ? [ '[', ']' ]
       : [ propertySeparator, '' ]
+  const [ startCoverUpProperty, closeCoverUpProperty ] = Array.isArray( coverUpProperty ) && coverUpProperty.length === 2
+    ? coverUpProperty
+    : typeof coverUpProperty === 'string'
+      ? [ coverUpProperty, coverUpProperty ]
+      : [ '', '' ]
 
   return Object.entries( dot ).reduce( ( record, [ key, value ] ) => {
-    const dotkey = isArray ? `${startArrayBracket}${key}${closeArrayBracket}` : `${key}`
+    const dotkey = isArray ? `${startArrayBracket}${key}${closeArrayBracket}` : `${startCoverUpProperty}${key}${closeCoverUpProperty}`
     const separator = Array.isArray( value ) ? '' : propertySeparator
 
     if ( typeof value === 'object' && value !== null )
